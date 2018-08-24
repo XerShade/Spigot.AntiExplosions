8b841e0cc05037f9338c3cced7fad6b6c0bca72b
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2018-08-24 06:33:13 -0400

    Added configuration settings.

--- src/main/java/ca/xershade/antiexplosions/AntiExplosions.java
+++ src/main/java/ca/xershade/antiexplosions/AntiExplosions.java
@@ -1,13 +1,23 @@
 package ca.xershade.antiexplosions;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.EntityType;
 import ca.xershade.antiexplosions.listeners.EntityExplodeListener;
 
 public final class AntiExplosions extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        // Register the listener.
-        getServer().getPluginManager().registerEvents(new EntityExplodeListener(), this);
+        // Generate configuration defaults.
+        for(EntityType t : EntityType.values()) {
+            // Assign the default value of false ot the entity type.
+            getConfig().addDefault(t.name(), false);
+        }
+
+        // Now save the defaults.
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+
+        // Register the listeners.
+        getServer().getPluginManager().registerEvents(new EntityExplodeListener(this), this);
     }
 }
--- src/main/java/ca/xershade/antiexplosions/listeners/EntityExplodeListener.java
+++ src/main/java/ca/xershade/antiexplosions/listeners/EntityExplodeListener.java
@@ -3,18 +3,26 @@ package ca.xershade.antiexplosions.listeners;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class EntityExplodeListener implements Listener {
+    private final JavaPlugin plugin;
+
+    public EntityExplodeListener(JavaPlugin plugin) {
+        // Store a reference to the plugin.
+        this.plugin = plugin;
+    }
+
     @EventHandler
     public void onEntityExplode(EntityExplodeEvent event)
     {
-        // Check to see if it is already canceled.
-        if(!event.isCancelled()) {
-            // It's not, change that.
-            event.setCancelled(true);
-
-            // Also make other stuff zero to ensure it's canceled.
-            event.setYield(0);
+        // Check to see if the entity is not allowed to explode.
+        if (!plugin.getConfig().getBoolean(event.getEntityType().name(), false)) {
+            // Check to see if the event is canceled.
+            if(!event.isCancelled()) {
+                // We don't want this to happen, cancel it.
+                event.setCancelled(true);
+            }
         }
     }
 }
